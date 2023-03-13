4c6f05e497f16dc8a3b662baee20f4ec3c749f39
golfzaa7 <vorravoot_n@hotmail.com>
2023-03-13 08:14:35 +0700

    update method

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -23,103 +23,62 @@ ScalarConverter::~ScalarConverter(void)
 void ScalarConverter::convert(const std::string& input)
 {
     this->_input = input;
-    std::stringstream ss(input);
-    ss >> this->_double;
+    // std::stringstream ss(input);
+    // ss >> this->_double;
 
-    if (caseChar())
+    // std::cout << "strstream : " << ss.str() << std::endl;
+    // std::cout << "value : " << this->_double << std::endl;
+    // std::cout << "fail : " << ss.fail() << std::endl;
+
+    if (caseDouble())
+        return ;
+    if (caseFloat())
         return ;
     if (caseInt())
         return ;
-    if (caseFloat())
+    if (caseChar())
         return ;
 }
 
 bool ScalarConverter::caseChar(void)
 {
-    // if not char [ length != 1 ]. from example, 0 is shown as null, then it might be int first
-    if (this->_input.length() != 1 || std::isdigit(this->_input[0]))
-        return (false);
-
-    this->_char = this->_input[0];
-  
-    std::cout << "char case" << std::endl;
-    cprint(this->_char);
-    cprint(static_cast<int>(this->_char));
-    cprint(static_cast<float>(this->_char));
-    cprint(static_cast<double>(this->_char));
+    // if (this->_input.length() != 1 || std::isdigit(this->_input[0]))
+    //     return (false);
+    
+    // std::cout << "char case => " << this->_input[0] << std::endl;
     return (true);
 }
 
 bool ScalarConverter::caseInt(void)
 {
-    // try stringstream way
-    // if overflow from int, i decide to let it unbehavior
-    std::stringstream iss(this->_input);
-    iss >> this->_int;
-
-    if (iss.good() || (this->_int == 0 && this->_input[0] != '0'))
-        return (false);
-
-    std::cout << "int case" << std::endl;
-    cprint(static_cast<char>(this->_int));
-    cprint(this->_int);
-    cprint(static_cast<float>(this->_int));
-    cprint(static_cast<double>(this->_int));
+ 
     return (true);
 }
 
 bool ScalarConverter::caseFloat(void)
 {
-    if (this->_input.back() != 'f')
-        return (false);
-    std::stringstream fss(this->_input.substr(0, this->_input.length() - 1));
-    fss >> this->_float;
 
-    std::cout << this->_float << std::endl;
-    // std::cout << this->_float << std::endl;
-    // std::cout << fss.str()<< std::endl;
-
-    // std::cout << "float case" << std::endl;
     return (true);
 }
 
-void ScalarConverter::cprint(const char& c)
+bool ScalarConverter::caseDouble(void)
 {
-    std::cout << "char : ";
-    if (this->_double < CHAR_MIN || this->_double > CHAR_MAX)
-        std::cout << "impossible";
-    else if (std::isprint(c))
-        std::cout << "'" << c << "'";
-    else
-        std::cout << "Non printable";
-    std::cout << std::endl;
-}
+    std::stringstream ss(this->_input);
+    ss >> this->_double;
 
-void ScalarConverter::cprint(const int& i)
-{
-    std::cout << "int : ";
-    if (this->_double >= INT_MIN && this->_double <= INT_MAX)
-        std::cout << i;
-    else
-        std::cout << "impossible";
-    std::cout << std::endl;
-}
+    std::cout << this->_double << std::endl;
 
-void ScalarConverter::cprint(const float& f)
-{
-    std::cout << "float : ";
-    if (this->_double >= -FLT_MAX && this->_double <= FLT_MAX)
-        std::cout << std::fixed << std::setprecision(1) << f << "f";
-    else
-        std::cout << "impossible";
-    std::cout << std::endl;
-}
+    std::cout << "good : " << ss.good() << std::endl;
+    std::cout << "fail : " << ss.fail() << std::endl;
 
-void ScalarConverter::cprint(const double& d)
-{
-    std::cout << "double : ";
-    std::cout << d;
-    std::cout << std::endl;
+    if (ss.fail() || ss.good())
+    {
+        std::cout << "not double" << std::endl;
+        return (false);
+    }
+
+    std::cout << "double case" << std::endl;
+    return (true);
 }
 
 // bool ScalarConverter::checkNumber(void)
