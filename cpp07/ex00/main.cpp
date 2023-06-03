d67647353ebf465d65eb71ece2a4821c6d0363dc
golfzaa7 <vorravoot_n@hotmail.com>
2023-06-03 10:27:41 +0700

    adjust

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -4,41 +4,41 @@ int main(void)
 {
 	int a = 2;
 	int b = 3;
-	::swap(a, b);
+	swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
-	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	std::cout << "min( a, b ) = " << min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << max(a, b) << std::endl;
 	std::cout << std::endl;
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
-	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	std::cout << "min( c, d ) = " << min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << max(c, d) << std::endl;
 	std::cout << std::endl;
 
 	char y = 'y';
 	char z = 'z';
-	::swap(y, z);
+	swap(y, z);
 	std::cout << "y = " << y << ", z = " << z << std::endl;
-	std::cout << "min( y, z ) = " << ::min(y, z) << std::endl;
-	std::cout << "max( y, z ) = " << ::max(y, z) << std::endl;
+	std::cout << "min( y, z ) = " << min(y, z) << std::endl;
+	std::cout << "max( y, z ) = " << max(y, z) << std::endl;
 	std::cout << std::endl;
 
 	float e = 0.42f;
 	float f = 42.42f;
-	::swap(e, f);
+	swap(e, f);
 	std::cout << "e = " << e << ", f = " << f << std::endl;
-	std::cout << "min( e, f ) = " << ::min(e, f) << std::endl;
-	std::cout << "max( e, f ) = " << ::max(e, f) << std::endl;
+	std::cout << "min( e, f ) = " << min(e, f) << std::endl;
+	std::cout << "max( e, f ) = " << max(e, f) << std::endl;
 	std::cout << std::endl;
 	
 	double g = 0.42;
 	double h = 42.42;
-	::swap(g, h);
+	swap(g, h);
 	std::cout << "g = " << g << ", h = " << h << std::endl;
-	std::cout << "min( g, h ) = " << ::min(g, h) << std::endl;
-	std::cout << "max( g, h ) = " << ::max(g, h) << std::endl;
+	std::cout << "min( g, h ) = " << min(g, h) << std::endl;
+	std::cout << "max( g, h ) = " << max(g, h) << std::endl;
 	return 0;
 }
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,27 +6,27 @@ int main(void)
     {
         std::cout << "Test for char arr" << std::endl;
         char arr[5] = {'a', 'b', 'c', 'd', 'e'};
-        ::iter(arr, 5, &talk);
+        iter(arr, 5, &talk);
     }
     {
         std::cout << "Test for string arr" << std::endl;
         std::string arr[5] = {"1", "2", "3", "4", "5"};
-        ::iter(arr, 5, &talk);
+        iter(arr, 5, &talk);
     }
     {
         std::cout << "Test for int arr" << std::endl;
         int arr[5] = {1, 2, 3, 4, 5};
-        ::iter(arr, 5, &talk);
+        iter(arr, 5, &talk);
     }
     {
         std::cout << "Test for float arr" << std::endl;
         float arr[5] = {1.01f, 2.02f, 3.03f, 4.04f, 5.05f};
-        ::iter(arr, 5, &talk);
+        iter(arr, 5, &talk);
     }
     {
         std::cout << "Test for double arr" << std::endl;
         double arr[5] = {1.02, 2.04, 3.06, 4.08, 5.10};
-        ::iter(arr, 5, &talk);
+        iter(arr, 5, &talk);
     }
 	return 0;
 }
