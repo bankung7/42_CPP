eb63ada49f8477a35f13c673a87daea51890e1fc
vorravoot n <vorravoot_n@hotmail.com>
2023-07-29 11:48:06 +0700

    fixed cpp08/ex02

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,60 +1,60 @@
 #include <iostream>
 #include "MutantStack.hpp"
 #include <stack>
-#include <vector>
-
+#include <list>
 
 int main()
 {
+    // test from subject
     MutantStack<int> mstack;
 
-    mstack.push(0);
-    mstack.push(999);
-    
-    std::cout << "the top of stack : " << mstack.top() << std::endl;
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << "top stack value : " << mstack.top() << std::endl;
 
     mstack.pop();
 
-    std::cout << "Current size : " << mstack.size() << std::endl;
-    std::cout << "the top of stack : " << mstack.top() << std::endl;
+    std::cout << "top stack value : " << mstack.top() << std::endl;
+    std::cout << "stack size " << mstack.size() << std::endl;
 
-    mstack.push(1);
-    mstack.push(2);
     mstack.push(3);
-    mstack.push(4);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
 
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
 
     ++it;
     --it;
+
     while (it != ite)
     {
-        std::cout << *it << std::endl;
+        std::cout << *it << " ";
         ++it;
     }
-    
-    std::cout << "==== Try with real stack ====" << std::endl;
-    std::stack<int> s(mstack);
-    std::cout << "Current stack size : " << mstack.size() << std::endl;
-    while (s.size() > 0)
-    {
-        std::cout << s.top() << std::endl;
-        s.pop();
-    }
-    std::cout << "Current stack size : " << mstack.size() << std::endl;
+    std::cout << std::endl;
 
-    std::cout << "==== Try copy by iterator to array ====" << std::endl;
-    std::vector<int> vec;
-    std::cout << "Vector size : " << vec.size() << std::endl;
-    it = mstack.begin();
-    while (it != ite)
-    {
-        vec.push_back(*it);
-        it++;
+    // real stack
+    std::stack<int> s(mstack);
+    // while (s.size() > 0)
+    // {
+    //     std::cout << s.top() << " ";
+    //     s.pop();
+    // }
+    // std::cout << std::endl;
+    
+    // try move to list
+    std::cout << "Move to list with iterator" << std::endl;
+    std::list<int> list;
+    list.insert(list.begin(), mstack.begin(), mstack.end());
+    std::list<int>::iterator lit = list.begin();
+    while (lit != list.end()) {
+        std::cout << *lit << " ";
+        lit++;
     }
-    std::cout << "Vector size : " << vec.size() << std::endl;
-    for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++)
-        std::cout << *it << std::endl;
+    std::cout << std::endl;
+
     return (0);
 }
